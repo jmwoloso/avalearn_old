621e7bf11f02f1390e4fe638b9c4caba4a349bca
Jason Wolosonovich <jmwoloso@asu.edu>
2016-03-07 18:13:44 -0700

    made changes to CheckChiSquareRequirements

--- avalearn/stats/diagnostic.py
+++ avalearn/stats/diagnostic.py
@@ -1,97 +1,109 @@
-# Authors: Jason Wolosonovich
+# Authors: Jason Wolosonovich <jmwoloso@asu.edu>
 # License: BSD 3 clause
 """
-dianostic.py:  Various diagnostics used to check if certain
+diagnostic.py:  Various diagnostics used to check if certain
                functions and classes from avalearn.stats.test can be
                used appropriately on one or more dataframes.
 """
 
 import numpy as np
+import pandas as pd
 from collections import OrderedDict
 import json
 from avalearn.utilities.functions import rounder
 
-class CheckChiSquareRequirements(object):
+class CheckChiSquareRequirements(OrderedDict):
+    # TODO: Need to calculate expected values for each in order to
+    # test requirement #2 accurately
     """
     Class used to verify that the supplied categorical features
     (2 or more) meet the requirements for the Chi-square
     goodness-of-fit test, which are:
 
         1. Fewer than 1/5 of the categories being compared have fewer
-           than 5 observations for each level, respectively.
+           than 5 expected observations for each level, respectively.
 
-        2. At least one observation is expected in every category level.
+        2. At least one observation is expected in every category
+        (i.e. no expected values of 0)
 
         3. Sampling method is simple random
 
-    NOTE: Regarding requirement #3, often we get datasets with no
+    Regarding requirement #3, often we get datasets with no
     information on the method  used to collect the samples (e.g.
     Kaggle competitions), therefore, this check may not be entirely
     reliable. Consider it one more data point that may provide
     clarity on which methods should/could be used for a given dataset.
 
-    NOTE: Regarding requirement #2, the only way to know whether or
-    not a dataset satisfies the requirement is check the levels for
-    each category in both the training data and the test data.
-
-    Sources:
-        https://statsmethods.wordpress.com/2013/05/29/chi-square-goodness-of-fit-test/
-        http://stattrek.com/chi-square-test/goodness-of-fit.aspx?Tutorial=AP
+    Regarding requirement #2: This refers to the EXPECTED values
 
     Parameters
     ----------
     train_dframe: pandas dataframe; required.
-        Training data.
+        Contains the training data.
 
     test_dframe: pandas dataframe; required.
-        Test data.
+        Contains the test data.
 
     categorical_columns: str, list or pandas.core.index.Index object; required.
         The categorical columns to analyze.
 
     Returns
     -------
-    nested collections.OrderedDict object containing columns as the
+    collections.OrderedDict object containing columns as the
     top-level keys.
 
-    Each top-level key contains a dict whose keys are
-    ['train', 'test'] and whose values are 'PASS' if there are no
-    differences between a column's category levels in the train and
-    test data, otherwise, the values are tuples of ('FAIL', columns)
-    where 'columns' represents the columns that are in the dataset
-    represented by the key (train|test) that are not present in the
-    other key's category levels for a given column.
+    The first two keys represent the overall result
+    for satisfying the two requirements that are being evaluated and
+    their corresponding values are in ['PASS', 'FAIL'].
 
-    Examples:
+    The value of the third key holds a dictionary where the keys are
+    in ['train', 'test'] and the value for each is a list containing
+    any columns in that failed evaluation of the first requirement.
 
-        If a column ['ColumnA'] passed the check, printing the value
-        for that column key would return:
+    The third key is there for convenience
+    (examing the failing columns) in case the first requirement is
+    not satisfied, but can also be useful even when the first
+    requirement is satisfied.
 
-        >>>diff_dict['ColumnA']
-        'PASS'
+    Each remaining key corresponds to the columns. These keys contain
+    dictionaries where the keys are in ['train', 'test'] and the
+    values are in ['PASS', ('FAIL', [])] with the failing category
+    levels for that feature populating the list inside the tuple.
 
-        Similarly, if ['ColumnB'] failed the check, printing the
-        value for that column key would return:
-
-        >>>diff_dict['ColumnB']
-        ('FAIL, ['LevelA', 'LevelB'])
-
+    Sources:
+        https://en.wikipedia.org/wiki/Pearson's_chi-squared_test
+        https://statsmethods.wordpress.com/2013/05/29/chi-square-goodness-of-fit-test/
+        http://stattrek.com/chi-square-test/goodness-of-fit.aspx?Tutorial=AP
     """
     # TODO: CheckChiSquareRequirements: Input validation
-    # TODO: CheckChiSquareRequirements: Make tests
+    # TODO: CheckChiSquareRequirements: Tests
+    # TODO: CheckChiSquareRequirements: Examples
+    # TODO: CheckChiSquareRequirements: Print json??
     def __init__(self, categorical_columns=None):
-        self.columns = pd.core.index.Index(categorical_columns)
-        self.total_columns = len(self.columns)
-        self.failing = 0
+        super(CheckChiSquareRequirements, self).__init__()
+        # type check
+        if not isinstance(categorical_columns, pd.Index):
+            self.columns = pd.Index(categorical_columns)
+        else:
+            self.columns = categorical_columns
+        self.column_count = len(self.columns)
+        self.failing_count = 0
         self.critical_value = rounder(self.column_count * 0.20)
+        self['Requirement_1'] = 'TBD'
+        self['Requirement_2'] = 'TBD'
+        self['Requirement_1_Failures'] = OrderedDict()
+        self['Requirement_1_Failures']['train'] = list()
+        self['Requirement_1_Failures']['test'] = list()
 
     def fit(self, train_dframe, test_dframe):
         """
         Create new dataframes with only the feature categories.
         """
-        # subset the dataframes
-        self.__train = train_dframe.loc[:, self.columns].copy()
-        self.__test = test_dframe.loc[:, self.columns].copy()
+        # subset the dataframes; replace missing
+        self.__train = train_dframe.loc[:, self.columns].fillna(
+            value='MISSING').copy()
+        self.__test = test_dframe.loc[:, self.columns].fillna(
+            value='MISSING').copy()
         return self
 
     def transform(self):
@@ -100,22 +112,40 @@ class CheckChiSquareRequirements(object):
         pass the check.
         """
         # TODO: Check if the instance has been fitted
-        # check assumption 2 above
+
+        # check requirements above
         for column in self.columns:
+            # evaluate requirement #1
+            self._train_unique_counts = self.__train.loc[:,
+                               column].value_counts()
+            self._test_unique_counts = self.__test.loc[:,
+                                       column].value_counts()
+
+            if self._train_unique_counts.min() < 5 \
+            or self._test_unique_counts.min() < 5:
+                self.failing_count += 1
+
+            if self._train_unique_counts.min() < 5:
+                self['Requirement_1_Failures']['train'].append(column)
+
+            if self._train_unique_counts.min() < 5:
+                self['Requirement_1_Failures']['test'].append(column)
+
+            # evaluate requirement #2
             self._train_unique = list(set(self.__train.loc[:,
                                           column].unique()))
             self._test_unique = list(set(self.__test.loc[:,
                                          column].unique()))
-            self._train_diff = np.setdiff1d(self._train_unique,
-                                            test_unique)
-            self._test_diff = np.setdiff1d(self._test_unique,
-                                           train_unique)
+            self._train_diff = list(np.setdiff1d(self._train_unique,
+                                                 self._test_unique))
+            self._test_diff = list(np.setdiff1d(self._test_unique,
+                                                self._train_unique))
 
             self[column] = OrderedDict()
             self[column]['train'] = None
             self[column]['test'] = None
 
-            if len(self._train_diff) and len(self._test_diff) == 0:
+            if len(self._train_diff) == 0 and len(self._test_diff) == 0:
                 self[column]['train'] = 'PASS'
                 self[column]['test'] = 'PASS'
 
@@ -127,7 +157,7 @@ class CheckChiSquareRequirements(object):
 
             if len(self._test_diff) != 0 \
                     and len(self._train_diff) == 0:
-                self['train'] = 'PASS'
+                self[column]['train'] = 'PASS'
                 self[column]['test'] = ['FAIL',
                                         self._test_diff]
 
@@ -137,6 +167,8 @@ class CheckChiSquareRequirements(object):
                                          self._train_diff]
                 self[column]['test'] = ['FAIL',
                                         self._test_diff]
+
+        self['Requirement_2'] = 'PASS'
         return self
 
     def fit_transform(self, train_dframe, test_dframe):
@@ -147,6 +179,7 @@ class CheckChiSquareRequirements(object):
         self.fit(train_dframe, test_dframe)
         self.transform()
         return self
+
 
 def check_simple_unimodal(dframe=None, column=None):
     """
